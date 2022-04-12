206cc1e2c01dabbd062d2560a86fffb2bdf8d668
Ricardo <ricardo.oliveira@prodepa.pa.gov.br>
2022-04-12 11:57:52 -0300

    Adicionar mensagem ao clicar no IonIcon de envio e scroll para a ultima mensagem

--- scripts.js
+++ scripts.js
@@ -1,4 +1,14 @@
-function testandoJs() {
-    console.log("Ola");
+let inputText = document.querySelector("#escreva-aqui"); //.value pega o texto digitado
+let chat = document.querySelector(".chat");
+let ultimaMensagem = document.querySelector(".mensagem:last-child");
+
+function enviarMensagem() {
+    
+    ultimaMensagem.scrollIntoView(); //Scrolla para ultima mensagem
+
+    chat.innerHTML += `<div class="mensagem">
+                            <p><span class="horario">(09:22:10) </span><strong>João</strong> para <strong>Todos</strong>: ${inputText.value}</p>
+                        </div>
+`
+    
 }
-testandoJs();
